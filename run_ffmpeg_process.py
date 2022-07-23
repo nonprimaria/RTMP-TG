b9e43419b62eed5a3b82533c63ac10c8c8de4ea8
nonprimaria <106350558+nonprimaria@users.noreply.github.com>
2022-07-23 22:03:57 +0600

    Update run_ffmpeg_process.py

--- run_ffmpeg_process.py
+++ run_ffmpeg_process.py
@@ -3,13 +3,13 @@ import shlex
 from config import *
 
 
-yt_installed = True
+_installed = True
 
 try:
-    import youtube_dl
-    from ytdl_exc_ import get_direct_link
-except ImportError: # ytdl is not installed
-    yt_installed = False
+    import 
+    from _exc_ import get_direct_link
+except ImportError: #  is not installed
+    _installed = False
 
 
 async def run_cmd(cmd: str):
@@ -20,7 +20,7 @@ async def run_cmd(cmd: str):
 
 async def ffmpeg_rtmp_stream(url, key, to_stream):
     """Streams a video from a rtmp server to a ffmpeg process."""
-    if yt_installed:
+    if _installed:
         url = await get_direct_link(url) # bad method but yes. it is fine lol
     cmd_ = f'-stream_loop -1 -re -i "{to_stream}" -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 128k -ac 2 -ar 44100 -f flv {url}{key}'
     process_ = await run_cmd(cmd_)
